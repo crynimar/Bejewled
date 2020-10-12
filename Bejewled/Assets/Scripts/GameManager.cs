99e1cbc916ffde36b58a7bc6e4a67b045e06b5c8
dalmar <dalmar.duraes@gmail.com>
2020-10-11 22:05:55 -0300

    Create game manager script and piece prefab
    Signed-off-by: dalmar <dalmar.duraes@gmail.com>

---
+++ Bejewled/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        //Create Singleton instance
+        if (Instance == null || Instance != this)
+            Instance = this; 
+    }
+}
---
+++ Bejewled/Assets/Scripts/Piece.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI ;
+
+public class Piece : MonoBehaviour
+{
+    [SerializeField] private Sprite[] PossibleSprites;
+    [SerializeField] private Image ImageComponent;
+
+    private Sprite CurrentSprite;
+
+    private void Start()
+    {
+        ChooseSprite();
+    }
+    private void ChooseSprite()
+    {
+        CurrentSprite = PossibleSprites[Random.Range(0, PossibleSprites.Length)];
+        ImageComponent.sprite = CurrentSprite;
+    }
+}
